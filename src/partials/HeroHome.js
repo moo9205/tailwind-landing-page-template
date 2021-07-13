c6898092c9f82d487affdf5d6abe531115f324b6
moo9205 <angus920514@gmail.com>
2021-07-13 10:12:40 +0900

    changed

--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -46,7 +46,7 @@ function HeroHome() {
               className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
               data-aos="zoom-y-out"
             >
-              Make this website{" "}
+              Welcome to visit{" "}
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                 PIZZAHAND
               </span>
